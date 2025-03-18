96c3678185124154ee1eaa69e8072671e3ba1e95
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-03-18 16:19:45 +0100

    lab1: change name inti "lab2" and rearranged files

---
+++ lab2_ex1&2/target/debug/lab1.d
@@ -0,0 +1,1 @@
+C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\lab1\target\debug\lab1.exe: C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\lab1\src\main.rs
