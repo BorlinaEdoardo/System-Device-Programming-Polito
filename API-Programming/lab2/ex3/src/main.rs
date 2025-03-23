15a1193893decf3b931d8dc3443f1adf5355b29e
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-03-23 18:47:01 +0100

    Lab 2 ex 3: basic project setup

--- API-Programming/lab2/ex3/src/main.rs
+++ API-Programming/lab2/ex3/src/main.rs
@@ -1,3 +1,41 @@
+const BSIZE: usize = 20;
+pub struct Board {
+    boats: [u8; 4],
+    data: [[u8; BSIZE]; BSIZE],
+}
+pub enum Error {
+    Overlap,
+    OutOfBounds,
+    BoatCount,
+}
+pub enum Boat {
+    Vertical(usize),
+    Horizontal(usize)
+}
+impl Board {
+    /** Create a new board with the respective available ships */
+    pub fn new(boats: &[u8]) -> Board {
+
+    }
+
+    /* create a new board with a string representing the whole content of board.txt file */
+    pub fn from(s: String)->Board {
+
+    }
+
+    /* add a boat to the board if possible, return  */
+    pub fn add_boat(&mut self, boat: Boat, pos: (usize, usize)) -> Result<&mut Self, Error> {
+
+    }
+
+    /* converte la board in una stringa salvabile su file */
+    pub fn to_string(&self) -> String
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
+
+
--- API-Programming/lab2/ex3/target/debug/ex3.d
+++ API-Programming/lab2/ex3/target/debug/ex3.d
@@ -1,1 +1,1 @@
-C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\lab2\ex3\target\debug\ex3.exe: C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\lab2\ex3\src\main.rs
+C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\API-Programming\lab2\ex3\target\debug\ex3.exe: C:\Users\edoar\OneDrive\ -\ Politecnico\ di\ Torino\Poli\4QuartoAnno\PDS\System-Device-Programming-Polito\API-Programming\lab2\ex3\src\main.rs
