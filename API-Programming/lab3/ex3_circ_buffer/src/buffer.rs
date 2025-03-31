8a33d0b341f5106edf261e047591296ea1827d76
BorlinaEdoardo <edoardo.borlina@gmail.com>
2025-03-31 22:41:55 +0200

    lab 3 ex 3: complete read and write functions and fix constrains check conditions

--- API-Programming/lab3/ex3_circ_buffer/src/buffer.rs
+++ API-Programming/lab3/ex3_circ_buffer/src/buffer.rs
@@ -1,7 +1,8 @@
 pub struct CircularBuffer<T>{
     head: usize,
     tail: usize,
-    buffer: Vec<T>
+    buffer: Vec<T>,
+    num: usize
 }
 
 #[derive(Debug)]
@@ -13,20 +14,28 @@ impl<T> CircularBuffer<T>{
         CircularBuffer{
             head: 0,
             tail: 0,
-            buffer: Vec::with_capacity(capacity)
+            buffer: Vec::with_capacity(capacity),
+            num: 0
         }
     }
     pub fn write(&mut self, item: T) -> Result<(), StructError>  {
-        self.buffer[self.tail] = item;
-        self.tail += 1;
-        Ok(())
+        // if buffer is full error is returned
+        if self.size() == self.num {
+            Err(StructError::GenericError("Circular Buffer Overflow".to_string()))
+        } else {
+            self.buffer[self.tail] = item;
+            self.tail = (self.tail + 1) % self.size();
+            self.num += 1;
+            Ok(())
+        }
     }
     pub fn read(&mut self) -> Option(T) {
         let mut res;
         let buf = &self.buffer;
-        if self.head < self.head { // placeholder condition tbc
+        if self.num != 0 {
             res = Some(*buf[self.head]);
-            self.head += 1;
+            self.head = (self.head + 1) % self.size();
+            self.num += 1;
         } else {
             res = None;
         }
